2b8796a68a3032758f529a2a24ed9932650ef856
juan meisus <130049281+juanmeisus@users.noreply.github.com>
2024-02-20 20:37:48 +0000

    cambios finales

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { name } from "file-loader";
 import "../style/index.css";
 
 /**
@@ -33,9 +34,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${variables.name}</h1>
+          <h2>${variables.lastName}</h2>
+          <h3>${variables.role}</h3>
+          <h3>${variables.country}</h3>  
+          <h3>${variables.city}</h3>  
+          
+         
+
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
@@ -56,19 +62,21 @@ window.onload = function() {
     // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://media.licdn.com/dms/image/D4E03AQGk51Qip5RcGA/profile-displayphoto-shrink_200_200/0/1695674139532?e=1714003200&v=beta&t=K5HOowILj2CSmKy7OhF33rdiHaxh2yR5YM4XY_nV--w",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: null,
-    linkedin: null,
+    github: "https://github.com/juanmeisus",
+    linkedin:
+      "https://www.linkedin.com/in/juan-manuel-meisus-alvarez-b7b293280/",
     instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
+    name: "Lucy",
+    lastName: "Boilet",
+    role: "Web developer",
+    country: "EEUU",
+    city: "Miami"
   };
   render(window.variables); // render the card for the first time
 
